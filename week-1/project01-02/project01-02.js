f32755ef2fb334fd227c079ee95b8deec65f7201
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-08-12 15:56:37 -0400

    feat: add variables for service name and speed

---
+++ week-1/project01-02/project01-02.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Jarren Bess
+    Date: Aug 12, 2025
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic",
+  service2Name = "Express",
+  service3Name = "Extreme",
+  service4Name = "Ultimate";
+
+let service1Speed = "0 Mbps",
+  service2Speed = "100 Mbps",
+  service3Speed = "500 Mbps",
+  service4Speed = "1 Gig";
