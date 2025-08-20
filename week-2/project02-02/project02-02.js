a28e589b21f35cbd6aacc8a3700ad65a8a5ad827
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-08-19 23:03:01 -0400

    docs: update block header name and date

---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,10 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Jarren Bess
+      Date: Aug 19, 2025
+
+      Filename: project02-02.js
+ */
