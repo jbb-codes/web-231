33a2f25880e9e6fc3b32fa359e4dc91efd696d39
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-08-19 23:05:19 -0400

    feat: verify all form fields are filled out before submitting

--- week-2/project02-02/project02-02.js
+++ week-2/project02-02/project02-02.js
@@ -8,3 +8,16 @@
 
       Filename: project02-02.js
  */
+
+// Verify all form fields are filled out
+function verifyForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+  name && email && phone
+    ? window.alert("Thank you!")
+    : window.alert("Please fill in all fields.");
+}
+
+document.getElementById("submit").onclick = verifyForm;
